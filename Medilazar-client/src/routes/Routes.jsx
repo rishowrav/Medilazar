adab09052d4a5b9e94b863e3660797f51cda5c05
rishowrav <rishowrav@gmail.com>
2024-07-07 09:47:10 +0600

    add: navbar

--- Medilazar-client/src/routes/Routes.jsx
+++ Medilazar-client/src/routes/Routes.jsx
@@ -1,9 +1,39 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Main from "../layout/Main";
+import About from "../pages/About/About";
+import Shop from "../pages/Shop/Shop";
+import Blogs from "../pages/Blogs/Blogs";
+import Contact from "../pages/Contact/Contact";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        element: <About />,
+        path: "/about",
+      },
+      {
+        element: <Shop />,
+        path: "/shop",
+      },
+      {
+        element: <Blogs />,
+        path: "/blogs",
+      },
+      {
+        element: <Contact />,
+        path: "/contact",
+      },
+    ],
   },
 ]);
 export default router;
